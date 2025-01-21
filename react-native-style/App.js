84d48e6dba5849324fb611fb38fe348f1710dc03
BB545 <black1016@hanmail.net>
2025-01-21 19:20:35 +0900

    study: 정렬 및 레이아웃

--- react-native-style/App.js
+++ react-native-style/App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import InlineStyle from './src/InlineStyle';
 import ClassStyle from './src/ClassStyle';
+import { Contents, Footer, Header } from './src/components/Layout';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <InlineStyle />
-      <ClassStyle />
+      <Header />
+      <Contents />
+      <Footer />
     </View>
   );
 }
