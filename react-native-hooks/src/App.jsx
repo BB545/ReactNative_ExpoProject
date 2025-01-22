2855ea00424482ef05374e84d5e965b2f69996ae
BB545 <black1016@hanmail.net>
2025-01-22 19:11:16 +0900

    study: useState, useEffect, useRef

---
+++ react-native-hooks/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import Counter from './components/Counter';
+import Form from './components/Form';
+import Button from './components/Button';
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #fff; 
+    justify-content: center;
+    align-items: center; 
+`;
+
+const App = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    return (
+        <Container>
+            <Button
+                title={isVisible ? 'Hide' : 'Show'}
+                onPress={() => setIsVisible(prev => !prev)}
+            />
+            {isVisible && <Form />}
+        </Container>
+    )
+}
+
+export default App
